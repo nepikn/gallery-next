08ac82f01fa0a8273a5866f8659caceb9b4970cc
nepikn <nepikn@gmail.com>
2023-09-26 16:48:49 +0800

    init

---
+++ app/preload-resources.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import ReactDOM from "react-dom";
+
+export function PreloadResources() {
+  ReactDOM.preload(
+    "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
+    { as: "style" }
+  );
+
+  return null;
+}
