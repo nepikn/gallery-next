bee484216a97aa59eb3dc9908aaed4dd01a9fdbf
nepikn <nepikn@gmail.com>
2023-10-05 19:39:14 +0800

    blurDataURL, photo caption

--- app/components/ExploreList.tsx
+++ app/components/ExploreList.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Buffer } from "node:buffer";
+import { getPlaiceholder } from "plaiceholder";
 import Figcaption from "./Figcaption";
 
 export default async function ExploreList() {
-  const liSpans = ["row-span-4", "row-span-5", "row-span-3", "row-span-3"];
   const photos = await getCollection();
+  const liSpans = [4, 5, 3, 3];
 
   return (
     <ul className="grid md:grid-cols-2 md:gap-x-4 lg:auto-rows-[6.25rem] lg:grid-cols-3">
       {photos.map((photo, i) => (
-        <li key={photo.id} className={"mb-8 " + liSpans[i]}>
+        <li key={photo.id} className={`mb-8 row-span-${liSpans[i]}`}>
           <ExploreFigure photo={photo} />
         </li>
       ))}
@@ -27,7 +29,12 @@ export async function getCollection(): Promise<Photo[]> {
       },
     }
   );
-  return res.json();
+
+  return (await res.json()).map((p: any) =>
+    Object.assign(p, {
+      caption: (p.description || p.alt_description).replace(/\s*-.*/, ""),
+    })
+  );
 }
 
 export function ExploreFigure({ photo }: { photo: Photo }) {
@@ -36,7 +43,7 @@ export function ExploreFigure({ photo }: { photo: Photo }) {
       <Figcaption
         className="[&_h3]:text-3xl w-full absolute bottom-0 right-0 z-10 hidden p-6 text-right group-hover:block"
         {...{
-          caption: photo.description || photo.alt_description,
+          caption: photo.caption,
           author: photo.user.name,
           url: photo.user.links.html,
         }}
@@ -48,14 +55,14 @@ export function ExploreFigure({ photo }: { photo: Photo }) {
   );
 }
 
-export function ExploreImg({
+export async function ExploreImg({
   photo,
-  quality = "regular",
   className,
+  quality = "regular",
 }: {
   photo: Photo;
-  quality?: keyof Photo["urls"];
   className?: string;
+  quality?: keyof Photo["urls"];
 }) {
   return (
     <Image
@@ -64,13 +71,22 @@ export function ExploreImg({
       width={photo.width}
       height={photo.height}
       placeholder="blur"
-      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+      blurDataURL={await blurDataURL(photo)}
       className={`h-full w-full max-w-none object-cover ${className}`}
     />
   );
 }
 
+async function blurDataURL(photo: Photo) {
+  const res = await fetch(photo.urls.thumb);
+  const arrayBuffer = await res.arrayBuffer();
+  const { base64 } = await getPlaiceholder(Buffer.from(arrayBuffer));
+
+  return base64;
+}
+
 interface Photo {
+  caption: string;
   id: string;
   slug: string;
   created_at: string;
